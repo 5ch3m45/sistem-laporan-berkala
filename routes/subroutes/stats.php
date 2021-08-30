e3ba8d00abdec9cea885627652b5499e6ee31a0f
Maulana Ichwan Anshory <64744726+masiwang@users.noreply.github.com>
2021-08-30 17:56:40 +0700

    Routing terpusat
    REMOVE:
    - no need

--- routes/subroutes/stats.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\StatController;
-
-Route::get( '/perusahaan/{company}/statistik', [StatController::class, 'index'  ])->name('stats');
