e98fd43e9d15430c98a057a78cfa03cf2612a6a6
Maulana Ichwan Anshory <64744726+masiwang@users.noreply.github.com>
2021-08-30 17:59:01 +0700

    Update notes.blade.php
    FIX:
    - avatar nama alias
    
    ADD:
    - modal menghapus catatan

--- resources/views/company/notes.blade.php
+++ resources/views/company/notes.blade.php
@@ -33,12 +33,21 @@
                                 <div class="card">
                                     <div class="card-body">
                                         <div class="divide-y">
+                                            @php
+                                                function nameAlias($name) {
+                                                    $alias = array();
+                                                    foreach (explode(' ', $name) as $string) {
+                                                        array_push($alias, strtoupper($string[0]));
+                                                    }
+                                                    return implode($alias);
+                                                }
+                                            @endphp
                                             @if (count($company->notes) > 0)
                                             @foreach ($company->notes()->orderByDesc('id')->get() as $note)
                                             <div>
                                                 <div class="row">
                                                     <div class="col-auto">
-                                                        <span class="avatar">JL</span>
+                                                        <span class="avatar">{{ nameAlias($note->user->name) }}</span>
                                                     </div>
                                                     <div class="col">
                                                         <div class="text-truncate">
@@ -48,7 +57,7 @@
                                                         <div class="text-muted">{{ $note->created_at }}</div>
                                                     </div>
                                                     <div class="col-auto align-self-center">
-                                                        <div class="badge bg-primary"></div>
+                                                        <button class="btn btn-white btn-sm delete-modal-btn" data-id="{{ $note->id }}"><i class="fe fe-trash"></i></button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -144,4 +153,37 @@
         </div>
     </footer>
 </div>
+<div class="modal modal-blur fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header text-danger">
+                <h5 class="modal-title font-weight-bold" id="deleteModalLabel"><i class="fe fe-alert-triangle"></i> Hapus Berkas</h5>
+                <button type="button" class="btn btn-sm btn-white" data-bs-dismiss="modal" aria-label="Close">&times;</button>
+            </div>
+            <div class="modal-body">
+                <p>Apakah Anda yakin menghapus catatan ini?</p>
+                <form action="{{ route('delete_note') }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="hidden" name="note_id">
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="btn btn-light shadow-secondary mr-3" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-danger text-light shadow-danger">Ya</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('foot-js')
+    <script>
+        let deleteMdlBtn = $('.delete-modal-btn');
+        let deleteMdl = new bootstrap.Modal($('#deleteModal'))
+        deleteMdlBtn.on('click', function() {
+            $('input[name=note_id]').val($(this).data('id'))
+            deleteMdl.show()
+        })
+    </script>
 @endsection
