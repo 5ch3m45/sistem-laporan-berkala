d24dc99aef07119bdf6de9757ba73ac35e108523
Maulana Ichwan Anshory <64744726+masiwang@users.noreply.github.com>
2021-08-30 17:51:01 +0700

    Update Company.php
    ADD:
    - tambah kolom code
    - method generate stat untuk atribute company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -15,25 +15,29 @@ class Company extends Model
         'debt_to_equity', 
         'return_on_asset',
         'return_on_equity',
-        'bopo'
+        'bopo',
+        'asset_total',
+        'liability_total',
+        'equity_total'
     ];
 
     protected $fillable = [
-      'name',
-      'regional',
-      'outlet',
-      'add_postalcode',
-      'add_province',
-      'add_regency',
-      'add_subdistrict',
-      'add_village',
-      'add_road',
-      'email',
-      'phone',
-      'birthdate',
-      'lic_number',
-      'lic_date',
-      'tax_number',
+        'code',
+        'name',
+        'regional',
+        'outlet',
+        'add_postalcode',
+        'add_province',
+        'add_regency',
+        'add_subdistrict',
+        'add_village',
+        'add_road',
+        'email',
+        'phone',
+        'birthdate',
+        'lic_number',
+        'lic_date',
+        'tax_number',
     ];
 
     public function employes() {
@@ -61,9 +65,56 @@ class Company extends Model
         return $number/$divider;
     }
 
+    public function generateSingleStat($account) {
+        $latest_report = $this->reports()->orderByDesc('year')->orderByDesc('quarter')->first();
+        if(!$latest_report) {
+            return [
+                'yoy' => 0,
+                'qtq' => 0
+            ];
+        }
+        $last_year_report = $this->reports()->where('year', $latest_report->year - 1)->where('quarter', $latest_report->quarter)->first();
+        if(!$last_year_report || !$latest_report) {
+            return [
+                'yoy' => 0,
+                'qtq' => 0
+            ];
+        }
+        if($latest_report->quarter == 1) {
+            $last_quarter_report = $this->reports()->where('year', $latest_report->year - 1)->where('quarter', 4)->first();
+        } else {
+            $last_quarter_report = $this->reports()->where('year', $latest_report->year)->where('quarter', $latest_report->quarter - 1)->first();
+        }
+
+        $number_latest = $latest_report->data()->where('account_code', $account)->first();
+        $number_last_y = $last_year_report->data()->where('account_code', $account)->first();
+        $number_last_q = $last_quarter_report->data()->where('account_code', $account)->first();
+        
+        $latest = $number_latest->value;
+        $last_y = $number_last_y->value;
+        $last_q = $number_last_q->value;
+
+        return [
+            'yoy' => number_format($this->safeDivider($latest - $last_y, $last_y)*100, 2),
+            'qtq' => number_format($this->safeDivider($latest - $last_q, $last_q)*100, 2),
+        ];
+    }
+
     public function generateStat($number_account, $divider_account) {
         $latest_report = $this->reports()->orderByDesc('year')->orderByDesc('quarter')->first();
+        if(!$latest_report) {
+            return [
+                'yoy' => 0,
+                'qtq' => 0
+            ];
+        }
         $last_year_report = $this->reports()->where('year', $latest_report->year - 1)->where('quarter', $latest_report->quarter)->first();
+        if(!$last_year_report || !$latest_report) {
+            return [
+                'yoy' => 0,
+                'qtq' => 0
+            ];
+        }
         if($latest_report->quarter == 1) {
             $last_quarter_report = $this->reports()->where('year', $latest_report->year - 1)->where('quarter', 4)->first();
         } else {
@@ -110,5 +161,17 @@ class Company extends Model
 
     public function getBopoAttribute() {
         return $this->generateStat('LRK_B_TOTAL_BO', 'LRK_P_TOTAL_PO');
+    }
+
+    public function getAssetTotalAttribute() {
+        return $this->generateSingleStat('LPK_A_TOTAL');
+    }
+
+    public function getLiabilityTotalAttribute() {
+        return $this->generateSingleStat('LPK_L_TOTAL');
+    }
+
+    public function getEquityTotalAttribute() {
+        return $this->generateSingleStat('LPK_E_TOTAL');
     }
 }
